c0685ca95aa72e14ced9a16d38e1ccdf44acdc93
Djamalutin <fisher.gerryy@mail.ru>
2023-05-23 12:50:26 +0300

    feat: component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const App = () => <p>Hi</p>;
+const App = () => (
+  <div>
+    <HookSwitcher></HookSwitcher>
+  </div>
+);
+
+const HookSwitcher = () => {
+  return (
+    <div style={{ padding: "10px", backgroundColor: "white" }}>
+      <button onClick={() => {}}>Dark</button>
+      <button onClick={() => {}}>Light</button>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
