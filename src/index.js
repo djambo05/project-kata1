b0e8faf04b9a2759bcc4018cd0749d187ac16937
Djamalutin <fisher.gerryy@mail.ru>
2023-05-23 16:50:19 +0300

    feat: useMemo and  useCallback

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback, useMemo } from "react";
 import ReactDOM from "react-dom/client";
 
 const App = () => {
@@ -18,12 +18,29 @@ const App = () => {
 };
 
 const useRequest = (request) => {
-  const [dataState, setDataState] = useState(null);
+  const initialState = useMemo(
+    () => ({
+      data: null,
+      loading: true,
+      error: null,
+    }),
+    []
+  );
+  const [dataState, setDataState] = useState(initialState);
   useEffect(() => {
+    setDataState(initialState);
     let cancelled = false;
-    request().then((data) => !cancelled && setDataState(data));
+    request()
+      .then(
+        (data) =>
+          !cancelled && setDataState({ data, loading: false, error: null })
+      )
+      .catch(
+        (error) =>
+          !cancelled && setDataState({ data: null, loading: false, error })
+      );
     return () => (cancelled = true);
-  }, [request]);
+  }, [request, initialState]);
   return dataState;
 };
 
@@ -39,7 +56,13 @@ const usePlanetInfo = (id) => {
 };
 
 const PlanetInfo = ({ id }) => {
-  const data = usePlanetInfo(id);
+  const { data, loading, error } = usePlanetInfo(id);
+  if (error) {
+    return <div>Something is wrong</div>;
+  }
+  if (loading) {
+    return <div>Loading</div>;
+  }
   return (
     <div>
       {id} - {data && data.name}
